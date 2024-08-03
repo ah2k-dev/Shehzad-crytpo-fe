a6faf4808803f6298a0d13d9afacd9880d86dc58
umer memon <104222183+ZubairArif234@users.noreply.github.com>
2024-08-03 10:39:33 +0500

    category added

--- src/data/categoryMenu.js
+++ src/data/categoryMenu.js
@@ -4,16 +4,43 @@ export const CategoryMenu = [
     subCategory: ["Hp", "Dell", "Lenovo", "Apple", "Realme", "Vivo", "Techo"],
   },
   {
-    label:"Fashion & beauty",
-    subCategory:[
-        "Shirts",
-        "T-shirts",
-        "Watches",
-        "Perfume",
-        "Shoes",
-        "Makeup",
-        "Hair extention",
-
-    ]
-  }
+    label: "Fashion & beauty",
+    subCategory: [
+      "Shirts",
+      "T-shirts",
+      "Watches",
+      "Perfume",
+      "Shoes",
+      "Makeup",
+      "Hair extention",
+    ],
+  },
+  {
+    label: "Furniture",
+    subCategory: [
+      "Bed",
+      "Sofa",
+      "Chairs",
+      "Showcase",
+      "Cupboard",
+      "Dressing table",
+      "Lamps",
+      "Dining table",
+      "Side table",
+    ],
+  },
+  {
+    label: "Electronics",
+    subCategory: [
+      "TV",
+      "Refrigerator",
+      "Air conditioner",
+      "Room cooler",
+      "Heater",
+      "Electric stove",
+      "Electric jug",
+      "Bulbs",
+      "Fans",
+    ],
+  },
 ];
