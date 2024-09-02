dd3ec344e153af01e68c8706fe0fd23cb974f7e1
Ahmed Hassan <ah2k.dev@gmail.com>
2024-09-02 20:54:05 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import "./App.css";
-import Router from "./router/router"
+import Router from "./router/router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/swiper-bundle.css';
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/swiper-bundle.css";
 import { themeStore } from "./stores/themeStore.jsx";
-
 
 const client = new QueryClient();
 
 function App() {
-
-const theme = themeStore((state) => state.theme)
-
-
-
+  const theme = themeStore((state) => state.theme);
 
   return (
     <div className={theme}>
-
-    <QueryClientProvider client={client}>
-      
-      <Router />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+      <QueryClientProvider client={client}>
+        <Router />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </div>
   );
 }
